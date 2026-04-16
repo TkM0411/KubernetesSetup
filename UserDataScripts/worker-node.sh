8f6154cbfdb15e900df6abd912ae75fccd7204a1
Tushar Kanti Mishra <tushar.mishra1165@gmail.com>
2026-04-16 19:55:19 +0530

    Refactored the worker node user data script.

--- UserDataScripts/worker-node.sh
+++ UserDataScripts/worker-node.sh
@@ -1,4 +1,13 @@
 #!/bin/bash -eux
 export PROJECT="k8nsetup"
-JOIN_COMMAND=$(aws ssm get-parameter --name "/${PROJECT}/kn8joincommand" --with-decryption --query "Parameter.Value" --output text)
-sudo eval "$JOIN_COMMAND"
+until JOIN_COMMAND=$(aws ssm get-parameter --name "/${PROJECT}/kn8joincommand" --with-decryption --query "Parameter.Value" --output text 2>/dev/null); do
+    echo "Waiting for join command..."
+    sleep 10
+done
+JOIN_COMMAND=$(echo "$JOIN_COMMAND" | tr -d '\r')
+
+echo "Executing join command..."
+echo "$JOIN_COMMAND" > /tmp/join.sh
+chmod +x /tmp/join.sh
+
+sudo /tmp/join.sh
--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,29 @@
 resource "aws_instance" "kubernetes_master_node" {
-    ami = data.aws_ssm_parameter.kubernetes_ec2_ami.value
-    vpc_id = data.aws_vpc.default_vpc.id
-    instance_type = var.ec2_instance_type
-    availability_zone = var.default_availability_zone
-    
+  ami                         = data.aws_ssm_parameter.kubernetes_ec2_ami.value
+  instance_type               = var.ec2_instance_type
+  availability_zone           = var.default_availability_zone
+  force_destroy               = true
+  hibernation                 = true
+  iam_instance_profile        = aws_iam_instance_profile.kubernetes_node_instance_profile.id
+  vpc_security_group_ids      = [aws_security_group.sg_kubernetes_nodes.id]
+  user_data                   = file("${path.module}/UserDataScripts/master-node.sh")
+  user_data_replace_on_change = true
+  tags = merge({
+    Name = "${var.project_name} Master Node"
+  }, local.common_tags)
+}
+
+resource "aws_instance" "kubernetes_worker_node" {
+  ami                         = data.aws_ssm_parameter.kubernetes_ec2_ami.value
+  instance_type               = var.ec2_instance_type
+  availability_zone           = var.default_availability_zone
+  force_destroy               = true
+  hibernation                 = true
+  iam_instance_profile        = aws_iam_instance_profile.kubernetes_node_instance_profile.id
+  vpc_security_group_ids      = [aws_security_group.sg_kubernetes_nodes.id]
+  user_data                   = file("${path.module}/UserDataScripts/worker-node.sh")
+  user_data_replace_on_change = true
+  tags = merge({
+    Name = "${var.project_name} Worker Node"
+  }, local.common_tags)
 }
